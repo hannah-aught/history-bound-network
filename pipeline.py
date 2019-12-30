2a2e0f3ef9408729502e61de56dcb5f16137e6ed
Hannah Brown <hsbrown@ucdavis.edu>
2019-12-29 22:02:17 -0800

    fixed node conditions and made them more compact by removing singleton clauses

--- pipeline.py
+++ pipeline.py
@@ -24,35 +24,35 @@ def parse_input(path):
     return mats
 
 
-def gen_tree_node_conditions(n, m, total_nodes):
+def gen_tree_node_conditions(n, m):
     # Each leaf must be included in the tree (I(l, k, l) = True)
-    root_i_clause = [1]
-    node_i_clause = list(range(2, n+m+2))
+    # root_i_clause = [1]
+    node_i_clause = list(range(1, n+m+1))
 
-    i_condition = Condition([root_i_clause] + [node_i_clause], True, n*m, total_nodes)
+    i_condition = Condition([node_i_clause], True, n*m, n+m)
 
-    leaf_i_condition = Condition(list(), True, m, n*total_nodes)
+    #leaf_i_condition = Condition(list(), True, m, n*total_nodes)
 
-    for l in range(n):
-        for i, val in enumerate(range(n+m+2, total_nodes+1)):
-            leaf_i_condition.add_clause([(-1 if i != l else 1) * (l * (total_nodes) + val)])
+    #for l in range(n):
+        #for i, val in enumerate(range(n+m+2, total_nodes+1)):
+            #leaf_i_condition.add_clause([(-1 if i != l else 1) * (l * (total_nodes) + val)])
 
-    final_i_val = n * m * total_nodes
+    final_i_val = n * m * (n+m)
 
-    root_t_condition = Condition([[final_i_val + 1]], True, m, total_nodes)
-    leaf_t_condition = Condition([[x + final_i_val + n + m + 2] for x in range(n)], True, m, total_nodes)
+    #root_t_condition = Condition([[final_i_val + 1]], True, m, total_nodes)
+    #leaf_t_condition = Condition([[x + final_i_val + n + m + 2] for x in range(n)], True, m, total_nodes)
     # m commodities and n+m internal nodes
-    internal_node_t_condition = Condition(list(), True, n+m, 1)
+    t_condition = Condition(list(), True, n+m, 1)
 
     for k in range(m):
         for l in range(n):
-            internal_node_t_condition.add_clause([-1 * (2 + (k*n+l)*total_nodes), final_i_val + 2 + k*total_nodes])
+            t_condition.add_clause([-1 * (1 + (k*n+l)*(n+m)), final_i_val + 1 + k*(n+m)])
     
-        internal_node_t_condition.add_clause([x for x in range(2, n*total_nodes+2, total_nodes)] + [-1 * (final_i_val + 2 + k*total_nodes)])
+        t_condition.add_clause([x for x in range(k*n*(n+m)+1,((k+1)*n - 1)*(n+m)+2, n+m)] + [-1 * (final_i_val + 1 + k*(n+m))])
     
-    node_conditions = [i_condition, leaf_i_condition, root_t_condition, internal_node_t_condition, leaf_t_condition]
+    node_conditions = [i_condition, t_condition]
 
-    max_val = final_i_val + m*total_nodes
+    max_val = final_i_val + m*(n+m)
 
     return node_conditions, max_val
 
@@ -61,13 +61,13 @@ def gen_tree_edge_conditions(node_conditions, n, m, total_nodes, num_node_vars):
 
     for j in range(1, m+n+1):
         for i in range(j):
-            f_condition.add_clause([j*n + 1, -1*(num_node_vars + j*(j-1)//2 + i + 1)])
-        f_condition.add_clause([-1*(j*n + 1)] + list(range(num_node_vars + j*(j-1)//2 + 1, num_node_vars + j*(j-1)//2 + i + 2)))
+            f_condition.add_clause([j + 1, -1*(num_node_vars + j*(j-1)//2 + i + 1)])
+        f_condition.add_clause([-1*(j + 1)] + list(range(num_node_vars + j*(j-1)//2 + 1, num_node_vars + j*(j-1)//2 + i + 2)))
 
-    for j in range(1, n+1):
-        for i in range(m+n):
-            f_condition.add_clause([(m+n+j)*n + 1, -1*(num_node_vars + (m+n)*(m+n-1)//2 + j * (m+n) + i + 1)])
-        f_condition.add_clause([-1*((m+n+j)*n + 1)] + list(range(num_node_vars + (m+n)*(m+n-1)//2 + j * (m+n) + 1, num_node_vars + (m+n)*(m+n-1)//2 + i + j * (m+n) + 2)))
+    leaf_f_condition = Condition(list(), True, m, total_nodes - 1)
+
+    for l in range(1, n+1):
+        leaf_f_condition.add_clause(list(range(num_node_vars + (m+n)*(m+n-1)//2 + l*(m+n) + 1, num_node_vars + (m+n)*(m+n-1)//2 + (l+1)*(m+n) + 1)))
 
     x_condition = Condition(list(), True, total_nodes*n*m, total_nodes - 1)
     edge_conditions = [f_condition, x_condition]
@@ -87,7 +87,7 @@ def gen_tree_conditions(input):
     # one edge from root to each internal node, all internal nodes connected (w/o cycles, so same as undirected handshake problem), one node from each internal node to each leaf
     num_edges = num_internal_nodes + (num_internal_nodes * (num_internal_nodes - 1))//2 + num_internal_nodes * n 
 
-    node_conditions, final_node_var = gen_tree_node_conditions(n, m, total_nodes)
+    node_conditions, final_node_var = gen_tree_node_conditions(n, m)
     edge_conditions, final_edge_var = gen_tree_edge_conditions(node_conditions, n, m, total_nodes, final_node_var)
 
     for l in range(n):
